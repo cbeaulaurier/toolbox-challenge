f4d61e802102dbce7cea1ce2afa580d4965efedd
Cassandra Beaulaurier <cassbeau@uw.edu>
2014-11-30 20:08:49 -0800

    Customized layout, working on functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 
 "use strict";
 
+var firstClicked;
+var tileBack = 'img/tile-back.png';
+
 $(document).ready(function() {
     var tiles = [];
     var idx;
 
+    var matched;
+    var missed;
+    var remaining = 8;
 
+    var matchedStats = $('#matched');
+    var missedStats = $('#missed');
+    var remainingStats = $('#remaining');
+
+    // create tiles array
     for (idx = 1; idx <= 32; ++idx) {
         tiles.push({
             tileNum: idx,
@@ -13,13 +24,16 @@ $(document).ready(function() {
         });
     }
 
+    // shuffle tiles
     console.log(tiles);
     var shuffledTiles = _.shuffle(tiles);
     console.log(shuffledTiles);
 
+    // select first 8 tiles
     var selectedTiles = shuffledTiles.slice(0, 8);
     console.log(selectedTiles);
 
+    // create pairs of selected tiles
     var tilePairs = [];
     _.forEach(selectedTiles, function(tile) {
         tilePairs.push(_.clone(tile));
@@ -28,7 +42,8 @@ $(document).ready(function() {
     tilePairs = _.shuffle(tilePairs);
     console.log(tilePairs);
 
-    var tileBack = 'img/tile-back.png'
+    // add selected tile pairs to game board
+
     var gameBoard = $('#game-board');
     var row = $(document.createElement('div'));
     var img;
@@ -37,7 +52,6 @@ $(document).ready(function() {
             gameBoard.append(row);
             row = $(document.createElement('div'));
         }
-
 
         img = $(document.createElement('img'));
         img.attr({
@@ -49,28 +63,52 @@ $(document).ready(function() {
     });
     gameBoard.append(row);
 
+    // on image click
     $('#game-board img').click(function() {
         var img = $(this);
-        var tile = img.data('tile');
-        img.fadeOut(100, function() {
-            if (tile.flipped) {
-                img.attr('src', tileBack);
-            } else {
-                img.attr('src', tile.src);
-            }
-            tile.flipped = !tile.flipped;
-            img.fadeIn(100);
-        }); // after fadeOut
+
+        if (!this.flipped) {
+            flipTile(img);
+        }
+        if (firstClicked) {     // if second phase of turn
+            if (firstClicked == this.tileNum)  {  // matched case
+                matched++;
+                matchedStats.innerHTML = "Matches: " + matched;
+                remaining--;
+                remainingStats.innerHTML = "Remaining: " + remaining;
+            } else {    // not matched
+                missed++;
+                missedStats.innerHTML = "Missed: " + missed;
+                window.setTimeout(function() {
+                    flipTile(img);
+                    flipTile(firstClicked);
+                }, 1000);
+                    this.flipped = !this.flipped;
+                }
+        } else {    // first phase of turn
+            this.flipped = !this.flipped;
+        }
     }); // on click of gameboard images
 
+
+    // create timer
     var startTime = _.now();
     var timer = window.setInterval(function() {
         var elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
-        $('#elapsed-seconds').text(elapsedSeconds);
-
-        if (elapsedSeconds >= 10) {
-            window.clearInterval(timer);
-        }
+        $('#elapsed-seconds').text("Timer: " + elapsedSeconds + "s");
     }, 1000);
-
 }); // jQuery ready function
+
+function flipTile(img) {
+    var tile = img.data('tile');
+    img.fadeOut(100, function() {
+        if (tile.flipped) {
+            img.attr('src', tileBack);
+            firstClicked = null;
+        } else {
+            img.attr('src', tile.src);
+            firstClicked = tile;
+        }
+        img.fadeIn(100);
+        });
+}
